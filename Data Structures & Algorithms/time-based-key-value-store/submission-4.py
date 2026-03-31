6b279d1232142e2e6520986b8d489551a821cc1a
Reed Colloton <36390235+reed-colloton@users.noreply.github.com>
2026-03-31 12:35:00 -0700

    Add: time-based-key-value-store - submission-4

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-4.py
@@ -0,0 +1,26 @@
+class TimeMap:
+
+    def __init__(self):
+        self.mp = collections.defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.mp[key].append((value, timestamp))
+
+    def get(self, key: str, timestamp: int) -> str:
+        times = self.mp[key]
+        if not times:
+            return ''
+        l, r = 0, len(times) - 1
+        while l <= r:
+            mid = l + (r - l) // 2
+            # if times[mid][1] == timestamp:
+            #     return times[mid][0]
+            if timestamp >= times[mid][1]:
+                l = mid + 1
+            else:
+                r = mid - 1
+        if times[r][1] <= timestamp:
+            return times[r][0]
+        else:
+            return ''
+        
