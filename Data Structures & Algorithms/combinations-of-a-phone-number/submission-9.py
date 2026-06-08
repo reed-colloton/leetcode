4a5052a0778a80bbf3cdac7be966a67b3c5409db
Reed Colloton <36390235+reed-colloton@users.noreply.github.com>
2026-06-07 23:14:45 -0700

    Add: combinations-of-a-phone-number - submission-9

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-9.py
@@ -0,0 +1,25 @@
+class Solution:
+    to_letter = {
+            '2': 'abc', 
+            '3': 'def', 
+            '4': 'ghi', 
+            '5': 'jkl',
+            '6': 'mno', 
+            '7': 'pqrs', 
+            '8': 'tuv', 
+            '9': 'wxyz',
+    }
+    def letterCombinations(self, digits: str) -> List[str]:
+        res = []
+        buffer = []
+        def dfs(i):
+            if i == len(digits):
+                res.append(''.join(buffer))
+                return
+            for char in self.to_letter[digits[i]]:
+                buffer.append(char)
+                dfs(i + 1)
+                buffer.pop()
+        if digits:
+            dfs(0)
+        return res
