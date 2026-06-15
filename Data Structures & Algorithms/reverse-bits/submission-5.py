f023a019ef44a25daf25382054c04a969a9e3f99
Reed Colloton <36390235+reed-colloton@users.noreply.github.com>
2026-06-14 22:19:02 -0400

    Add: reverse-bits - submission-5

---
+++ Data Structures & Algorithms/reverse-bits/submission-5.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        new = 0x0
+        for i in range(32):
+            xi = n & 0x1
+            n >>= 1
+            new <<= 1
+            new |= xi
+        return new
+        
+    def reverseBitsStr(self, n: int) -> int:
+        s = f'{n:032b}'
+        s = s[::-1]
+        return int(s, 2)
