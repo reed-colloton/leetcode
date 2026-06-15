2cafbd6d4b1078c5a78ab3786f729c671cb04c59
Reed Colloton <36390235+reed-colloton@users.noreply.github.com>
2026-06-14 23:20:38 -0400

    Add: reverse-integer - submission-7

---
+++ Data Structures & Algorithms/reverse-integer/submission-7.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        sign = x < 0
+        x = abs(x)
+        new = 0
+        while x:
+            new *= 10
+            new += x % 10
+            x = int(x / 10)
+        if new > (2 ** 31 - 1):
+            return 0
+        return (-1) ** sign * new
