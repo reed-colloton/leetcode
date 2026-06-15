6a936961681320a44317fdefe3a2470b4632f6ba
Reed Colloton <36390235+reed-colloton@users.noreply.github.com>
2026-06-14 21:46:35 -0400

    Add: sum-of-two-integers - submission-7

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-7.py
@@ -0,0 +1,22 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        mask = 0xFFFFFFFF
+        a &= mask
+        b &= mask
+        carry = 0x0
+        place = 0x1
+        new = 0x0
+        while a or b or carry:
+            x1 = a & 0x1
+            x2 = b & 0x1
+            y = x1 ^ x2 ^ carry
+            carry = x1 & x2 | (x1 | x2) & carry
+            if y:
+                new |= place
+            place <<= 1
+            a >>= 1
+            b >>= 1
+        new &= mask
+        if new > 0x7FFFFFFF:
+            return new - (mask + 1)
+        return new
