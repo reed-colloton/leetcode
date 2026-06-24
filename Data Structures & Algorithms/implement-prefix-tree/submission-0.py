f649720319db96fd33e4f33d0a8b4ba58326a487
Reed Colloton <36390235+reed-colloton@users.noreply.github.com>
2026-06-23 21:46:02 -0700

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self):
+        self.children = [None] * 26
+        self.is_leaf = False
+
+    def get(self, c: str) -> Node:
+        return self.children[ord(c) - ord('a')]
+
+class PrefixTree:
+    def __init__(self):
+        self.root = Node()
+
+    def insert(self, word: str) -> None:
+        cur = self.root
+        for c in word:
+            i = ord(c) - ord('a')
+            if cur.children[i] == None:
+                cur.children[i] = Node()
+            cur = cur.children[i]
+        cur.is_leaf = True
+
+
+    def search(self, word: str) -> bool:
+        cur = self.root
+        for c in word:
+            i = ord(c) - ord('a')
+            if cur.children[i] == None:
+                return False
+            cur = cur.children[i]
+        return cur.is_leaf
+
+
+    def startsWith(self, prefix: str) -> bool:
+        cur = self.root
+        for c in prefix:
+            i = ord(c) - ord('a')
+            if cur.children[i] == None:
+                return False
+            cur = cur.children[i]
+        return True
+
+        
