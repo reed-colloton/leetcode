4bca8fc440fca6cd44dc37ddab9997c51a5caa3e
Reed Colloton <36390235+reed-colloton@users.noreply.github.com>
2026-06-24 23:27:16 -0700

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.py
@@ -0,0 +1,31 @@
+class Node:
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+
+class WordDictionary:
+    def __init__(self):
+        self.root = Node()
+        
+    def addWord(self, word: str) -> None:
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                node.children[c] = Node()
+            node = node.children[c]
+        node.is_end = True
+
+    def search(self, word: str) -> bool:
+        return self.search_helper(word, 0, self.root)
+
+    def search_helper(self, word: str, i: int, node: Node) -> bool:
+        if i == len(word):
+            return node.is_end
+        if word[i] == '.':
+            for option in node.children:
+                if self.search_helper(word, i + 1, node.children[option]):
+                    return True
+        if word[i] not in node.children:
+            return False
+        node = node.children[word[i]]
+        return self.search_helper(word, i + 1, node)
