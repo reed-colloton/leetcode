1d90f61633ba4ae3dfc967903cd884c9d9adafde
Reed Colloton <36390235+reed-colloton@users.noreply.github.com>
2026-07-04 13:30:43 -0700

    Add: course-schedule - submission-4

---
+++ Data Structures & Algorithms/course-schedule/submission-4.py
@@ -0,0 +1,28 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+
+        graph = {}
+        for course, prereq in prerequisites:
+            if course not in graph:
+                graph[course] = []
+            graph[course].append(prereq)
+
+        path = set()
+
+        def cycle(course):
+            if course in path:
+                return True
+            if course not in graph:
+                return False
+            path.add(course)
+            for prereq in graph[course]:
+                if cycle(prereq):
+                    return True
+            path.remove(course)
+            graph[course] = []
+            return False
+
+        for course in graph:
+            if cycle(course):
+                return False
+        return True
