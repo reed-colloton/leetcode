2083f190567ae2df6b92e69bc2dd8f0d63b49b6b
Reed Colloton <36390235+reed-colloton@users.noreply.github.com>
2026-07-06 13:13:43 -0700

    Add: non-cyclical-number - submission-4

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-4.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        seen = set()
+        while n != 1:
+            if n in seen:
+                return False
+            seen.add(n)
+            new = 0
+            while n:
+                x = n % 10
+                new += x ** 2
+                n //= 10
+            n = new
+        return True
