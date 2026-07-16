906ea1f2f0d6731b593ff4de6cf5fb63436c7a4f
Reed Colloton <36390235+reed-colloton@users.noreply.github.com>
2026-07-16 16:00:20 -0700

    Add: spiral-matrix - submission-3

---
+++ Data Structures & Algorithms/spiral-matrix/submission-3.py
@@ -0,0 +1,33 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        
+        if not matrix or not matrix[0]:
+            return []
+        
+        traversal = []
+        M, N = len(matrix), len(matrix[0])
+        level = 0
+        
+        while 2 * level < M and 2 * level < N:
+            r, c = level, level
+            width = N - level * 2 - 1
+            height = M - level * 2 - 1
+            traversal.append(matrix[r][c])
+            for _ in range(width):
+                c += 1
+                traversal.append(matrix[r][c])
+            if height == 0:
+                break
+            for _ in range(height):
+                r += 1
+                traversal.append(matrix[r][c])
+            if width == 0:
+                break
+            for _ in range(width):
+                c -= 1
+                traversal.append(matrix[r][c])
+            for _ in range(height - 1):
+                r -= 1
+                traversal.append(matrix[r][c])
+            level += 1
+        return traversal
