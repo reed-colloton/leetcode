3ae32b1d682a471e1a472207de6cf76f2a9a76bd
Reed Colloton <36390235+reed-colloton@users.noreply.github.com>
2026-07-17 14:55:56 -0700

    Add: word-ladder - submission-6

---
+++ Data Structures & Algorithms/word-ladder/submission-6.py
@@ -0,0 +1,29 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        m = len(wordList[0])
+        graph = {word : [] for word in wordList + [beginWord]}
+        for word in graph:
+            for i in range(m):
+                for c in range(26):
+                    char = chr(ord('a') + c)
+                    neighboor = word[:i] + char + word[i + 1:]
+                    if neighboor in graph:
+                        graph[neighboor].append(word)
+
+        queue = deque()
+        visited = set()
+
+        def bfs():
+            if not queue:
+                return 0
+            word, step = queue.popleft()
+            if word == endWord:
+                return step
+            if word not in visited:
+                for neighboor in graph[word]:
+                    visited.add(word)
+                    queue.append((neighboor, step + 1))
+            return bfs()
+        
+        queue.append((beginWord, 1))
+        return bfs()
