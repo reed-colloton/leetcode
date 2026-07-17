cc81a39c334a48c546c204831e033c3264c5158f
Reed Colloton <36390235+reed-colloton@users.noreply.github.com>
2026-07-17 15:16:52 -0700

    Add: word-ladder - submission-25

---
+++ Data Structures & Algorithms/word-ladder/submission-25.py
@@ -0,0 +1,26 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+
+        words = set(wordList)
+
+        if endWord not in words or beginWord == endWord:
+            return 0
+
+        queue = deque([(beginWord, 1)])
+        visited = {beginWord}
+        m = len(beginWord)
+
+        while queue:
+            word, step = queue.popleft()
+
+            if word == endWord:
+                return step
+
+            for i in range(m):
+                for c in range(26):
+                    char = chr(ord('a') + c)
+                    neighbor = word[:i] + char + word[i + 1:]
+                    if neighbor in words:
+                        words.remove(neighbor)
+                        queue.append((neighbor, step + 1))
+        return 0
