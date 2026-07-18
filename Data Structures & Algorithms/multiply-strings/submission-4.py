0ae564c03979d8a2a95564fa142c6ec7b8fb96bf
Reed Colloton <36390235+reed-colloton@users.noreply.github.com>
2026-07-18 13:21:50 -0700

    Add: multiply-strings - submission-4

---
+++ Data Structures & Algorithms/multiply-strings/submission-4.py
@@ -0,0 +1,9 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        product = 0
+        for i in range(len(num1)):
+            x = int(num1[len(num1) - i - 1])
+            for j in range(len(num2)):
+                y = int(num2[len(num2) - j - 1])
+                product += (x * (10 ** i)) * (y * (10 ** j))
+        return str(product)
