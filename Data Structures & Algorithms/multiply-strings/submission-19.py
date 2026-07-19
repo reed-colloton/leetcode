5922bfff5f392c17087107a0e70f911f4384b756
Reed Colloton <36390235+reed-colloton@users.noreply.github.com>
2026-07-18 22:35:42 -0700

    Add: multiply-strings - submission-19

---
+++ Data Structures & Algorithms/multiply-strings/submission-19.py
@@ -0,0 +1,20 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+
+        if num1 == '0' or num2 == '0':
+            return '0'
+
+        x_digits = [int(x) for x in reversed(num1)]
+        y_digits = [int(y) for y in reversed(num2)]
+
+        product = 0
+        x_place = 1
+
+        for x in x_digits:
+            y_place = x_place
+            for y in y_digits:
+                product += x * y * y_place
+                y_place *= 10
+            x_place *= 10
+
+        return str(product)
