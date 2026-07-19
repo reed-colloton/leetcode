85fa649a54599ab1378f1102cef9e5f78adce899
Reed Colloton <36390235+reed-colloton@users.noreply.github.com>
2026-07-19 16:03:29 -0700

    Add: count-squares - submission-3

---
+++ Data Structures & Algorithms/count-squares/submission-3.py
@@ -0,0 +1,26 @@
+class CountSquares:
+    def __init__(self):
+        self.ys = defaultdict(list)
+        self.xs = defaultdict(list)
+        self.points = defaultdict(int)
+
+    def add(self, point: List[int]) -> None:
+        x, y = point
+        self.xs[y].append(x)
+        self.ys[x].append(y)
+        self.points[(x, y)] += 1
+        
+    def count(self, point: List[int]) -> int:
+        x, y = point
+        squares = 0
+        for xi in self.xs[y]:
+            if xi == x:
+                continue
+            for yi in self.ys[x]:
+                if yi == y:
+                    continue
+                if abs(yi - y) == abs(xi - x):
+                    if xi in self.xs[yi] and yi in self.ys[xi]:
+                        print(f'({x}, {y}); ({xi}, {y}); ({x}, {yi}); ({xi}, {yi})')
+                        squares += self.points[(xi, yi)]
+        return squares
