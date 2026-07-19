2779254c7e3302d22b830387c856b4caafffddb7
Reed Colloton <36390235+reed-colloton@users.noreply.github.com>
2026-07-18 22:31:56 -0700

    Add: multiply-strings - submission-16

---
+++ Data Structures & Algorithms/multiply-strings/submission-16.py
@@ -0,0 +1,15 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        if '0' in (num1, num2):
+            return '0'
+        product = 0
+        x_place = 1
+        for i, x in enumerate(reversed(num1)):
+            x = int(x)
+            y_place = x_place
+            for j, y in enumerate(reversed(num2)):
+                y = int(y)
+                product += x * y * y_place
+                y_place *= 10
+            x_place *= 10
+        return str(product)
