3705755a2c1d676fb942d4e33cc041ec8d364099
Reed Colloton <36390235+reed-colloton@users.noreply.github.com>
2026-07-20 11:30:40 -0700

    Add: climbing-stairs - submission-6

---
+++ Data Structures & Algorithms/climbing-stairs/submission-6.py
@@ -0,0 +1,18 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+        first, second = 1, 2
+        for i in range(2, n):
+            first, second = second, first + second
+        return second
+
+    def climbStairsFull(self, n: int) -> int:
+        if n <= 2:
+            return n
+        paths = [0] * n
+        paths[0] = 1
+        paths[1] = 2
+        for i in range(2, n):
+            paths[i] = paths[i - 1] + paths[i - 2]
+        return paths[n - 1]
