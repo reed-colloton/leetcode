18efe89eb10fbe1a41f2a100b7d9fc0c64f7bda9
Reed Colloton <36390235+reed-colloton@users.noreply.github.com>
2026-07-23 11:13:21 -0700

    Add: house-robber-ii - submission-8

---
+++ Data Structures & Algorithms/house-robber-ii/submission-8.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+
+        n = len(nums)
+
+        def helper(start, end):
+            adjacent = far = 0
+            for i in range(start, end):
+                adjacent, far = max(adjacent, nums[i] + far), adjacent
+            return adjacent
+
+        return max(nums[0], helper(0, n - 1), helper(1, n))
